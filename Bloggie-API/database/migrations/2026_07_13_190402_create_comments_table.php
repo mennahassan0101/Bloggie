d2f94be546330a18e3eaf6bf41458e5e7583884a
Menna Hassan <217995817+mennahassan0101@users.noreply.github.com>
2026-07-13 23:33:41 +0300

    Done migrations

---
+++ Bloggie-API/database/migrations/2026_07_13_190402_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')
+                  ->constrained()
+                  ->onDelete('cascade')
+                  ->index()
+                  ->name('fk_comments_post_id');
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->index()->name('fk_comments_user_id');
+            $table->text('content');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
